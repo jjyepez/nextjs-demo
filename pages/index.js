5847cc079a2a58b8c8ada9964a4ddca605894cea
jjyepez <jjyepez@gmail.com>
2018-08-01 04:27:30 -0400

    Avances parametros en el Link 2

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default class extends Component {
     static async getInitialProps(){
-        const url = `https://noesishosting.com/sw/loremdata/?a=rs&p=nombre,foto,estatus:10`
+        const url = `https://noesishosting.com/sw/loremdata/?a=rs&p=nombre,foto,estatus:30`
         const rsp = await fetch( url ).catch( e => console.log(e) )
         const data = await rsp.json().catch( e => console.log(e) )
         return { data:data.rs }
@@ -16,8 +16,8 @@ export default class extends Component {
                 <header>App</header>
                 <div className = "contenedor">
                     { data.map( el => (
-                        <Link href={ `/preview?img=${el.foto}` }>
-                            <a><img key={ Math.random() } src = { el.foto } /></a>
+                        <Link key={ Math.random() } href={ `/preview?img=${ el.foto }` }>
+                            <a><img src = { el.foto } /></a>
                         </Link>
                     ))}
                 </div>
@@ -32,16 +32,19 @@ export default class extends Component {
                     }
                     .contenedor{
                         display: grid;
-                        grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
-                        grid-gap: 10px;
+                        grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+                        grid-gap: 5px;
+                        padding: 5px;
                     }
                     img {
                         width:100%;
+                        height: 160px;
+                        object-fit: cover;
                     }
                     .page{
                         margin-top: 56px;
                         flex: 1;
-                        width:100vw;
+                        width:100%;
                         display: flex;
                         justify-content: center;
                         align-items: center;
